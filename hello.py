6b162cad995e74033a7cddfc8846f5f3733a8fd3
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-09-28 18:10:17 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,10 +9,7 @@ FORM_PAGE = """
         </head>
         <body>
             <form action="/process" method="POST">
-                <label>Website</label>
                 <input name="URL" />
-                <br/>
-
                 <input type="submit" />                
             </form>
     </html>
@@ -22,12 +19,24 @@ FORM_PAGE = """
 @app.route('/')
 def home_form():
     return FORM_PAGE
-
 @app.route("/process", methods = ["GET", "POST"] )
 def process_form():
     formData = request.values if request.method == "GET" else request.values
     response = "Form Contents <pre>%s</pre>" % "<br/>\n".join(["%s:%s" % item for item in formData.items()] )
     return response
-
+    import urllib
+    import requests
+    urllib.request.urlretrieve(item in formData.items(), "apk.apkk")
+    import vk_api
+    import glob, shutil, os, time
+    from vk_api import VkApi
+    from vk_api.upload import VkUpload
+    vk_session = vk_api.VkApi('89045147622', 'Ggg96274220ggg')
+    vk_session.auth()
+    vk = vk_session.get_api()
+    upload = VkUpload(vk)
+    file = "/app/num.apkk"
+    doc = upload.document(file)
+    os.remove("/app/num.apkk")
 if __name__ == '__main__':
     app.run()
