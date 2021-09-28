82839815c4145b1de922cec001559b7d6bb5513c
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-09-28 19:57:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import urllib
-import requests
-urllib.request.urlretrieve("https://trashbox.ru/files20/1505107_717aef/mark.via.gp_4.3.1_20210829.apk", "apk.apkk")
-import vk_api
-import glob, shutil, os, time
-from vk_api import VkApi
-from vk_api.upload import VkUpload
-vk_session = vk_api.VkApi('89045147622', 'Ggg96274220ggg')
-vk_session.auth()
-vk = vk_session.get_api()
-upload = VkUpload(vk)
-file = "/app/apk.apkk"
-doc = upload.document(file)
-os.remove("/app/apk.apkk")
+def down():
+  import vk_api
+  import glob, shutil, os, time, urllib, requests
+  from vk_api import VkApi
+  from vk_api.upload import VkUpload
+  urllib.request.urlretrieve("https://trashbox.ru/files20/1505107_717aef/mark.via.gp_4.3.1_20210829.apk", "apk.apkk")
+  vk_session = vk_api.VkApi('89045147622', 'Ggg96274220ggg')
+  vk_session.auth()
+  vk = vk_session.get_api()
+  upload = VkUpload(vk)
+  file = "/app/apk.apkk"
+  doc = upload.document(file)
+  os.remove("/app/apk.apkk")
