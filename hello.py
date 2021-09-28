c23cce0a2716457b1765e66dc5e1389095efa440
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-09-28 21:07:04 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,68 +1,29 @@
 from flask import Flask
-from string import Template
-import requests
-
-def is_url_ok(url):
-    return 200 == requests.head(url).status_code
-
-HTML_TEMPLATE = Template("""
-<!DOCTYPE html>
-<head>
-   <title>My Video App</title>
-   <style>
-      body{
-          text-align: center;
-          background-color: #FFAAAA;
-      }
-
-      iframe{
-          width: 80%;
-          height: 600px;
-          -webkit-animation: fun-function 2.5s linear infinite;
-          -webkit-animation-direction: alternate;
-          -moz-animation: fun-function 2.5s linear infinite;
-          -moz-animation-direction: alternate;
-      }
-
-      @-webkit-keyframes fun-function{
-        from{
-          -webkit-transform: rotate(0deg) skew(-30deg);
-        }
-        to{
-          -webkit-transform: rotate(360deg) skew(30deg);
-        }
-      }
-
-      @-moz-keyframes fun-function{
-        from{
-          -moz-transform: rotate(0deg) skew(-30deg);
-        }
-        to{
-          -moz-transform: rotate(360deg) skew(30deg);
-        }
-      }
-   </style>
-</head>
-<body>
-    <h2>${headline}</h2>
-    <iframe src="https://www.youtube.com/embed/${youtube_id}?autoplay=1" frameborder="0" allowfullscreen></iframe>
-</body>""")
-
+from flask import request
 app = Flask(__name__)
+
+FORM_PAGE = """
+    <html>
+        <head>
+            <title>Flask Form</title>
+        </head>
+        <body>
+            <form action="/process" method="POST">
+                <input name="URL" />
+                <input type="submit" />
+                <iframe src="https://github.com/harishvinukumar/Mini-Search-Engine" width="468" height="60" align="left"></iframe>
+            </form>
+    </html>
+"""
+
 @app.route('/')
-def homepage():
-    return "Hello world"
+def home_form():
+    return FORM_PAGE
 
-@app.route('/videos/<vid>')
-def videos(vid):
-    youtube_url = 'https://www.youtube.com/watch?v=' + vid
-    if True == is_url_ok(youtube_url):
-        headline_html = """<a href="{url}">YouTube video: {id}</a>""".format(url=youtube_url, id=vid)
-        all_html = HTML_TEMPLATE.substitute(headline=headline_html, youtube_id=vid)
-    else:
-        headline_html = """YouTube video <u>{id}</u> does not exist""".format(id=vid)
-        all_html = HTML_TEMPLATE.substitute(headline=headline_html, youtube_id='dQw4w9WgXcQ')
-    return all_html
-
+@app.route("/process", methods = ["GET", "POST"] )
+def process_form():
+    formData = request.values if request.method == "GET" else request.values
+    response = "Form Contents <pre>%s</pre>" % "<br/>\n".join(["%s:%s" % item for item in formData.items()] )
+    return response
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run()
