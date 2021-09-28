313ab921501df0ffcbe83dfcffcd187f04731356
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-09-28 17:55:48 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-#Normally this would be an external file like object, but here
-#it's inlined
 FORM_PAGE = """
     <html>
         <head>
@@ -11,30 +9,10 @@ FORM_PAGE = """
         </head>
         <body>
             <form action="/process" method="POST">
-                <label>Name</label>
-                <input name="user_name" />
+                <label>Website</label>
+                <input name="URL" />
                 <br/>
-                
-                <label>Age</label>
-                <input type="range" name="user_age" />
-                <br/>
-                
-                <fieldset>
-                    <legend>Drink preferences</legend>
-                
-                    <label>Coffee, Tea, water?</label><br/>
-                    <input type="checkbox" name="drink_type[]" value="coffee" /> Coffee  <br/>
-                    <input type="checkbox" name="drink_type[]" value="tea" /> Tea  <br/>
-                    <input type="checkbox" name="drink_type[]" value="water" /> Water  <br/>
-                    
-                    <label>Temperature</label><br />
-                    <input type="radio" name="drink_temp" value="hot" /> Hot <br/>
-                    <input type="radio" name="drink_temp" value="warm" /> Warm <br/>
-                    <input type="radio" name="drink_temp" value="Cold" /> Cold <br/>
-                    <input type="radio" name="drink_temp" value="Iced" /> Iced <br/>
-                </fieldset>
-                    
-                
+
                 <input type="submit" />                
             </form>
     </html>
