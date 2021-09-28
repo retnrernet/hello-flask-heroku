cc658903c1367840276cac37b9381156170043e9
retnrernet <49436790+retnrernet@users.noreply.github.com>
2021-09-28 20:13:06 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,30 +1,60 @@
-from flask import Flask
-from flask import request
-import main
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
+import time
+
+from flask import Flask, Response, request
+
+from selenium.webdriver import PhantomJS
+
 app = Flask(__name__)
 
-FORM_PAGE = """
-    <html>
-        <head>
-            <title>Flask Form</title>
-        </head>
-        <body>
-            <form action="/process" method="POST">
-                <input name="URL" />
-                <input type="submit" />                
-            </form>
-    </html>
-"""
+
+class StderrLog(object):
+    def close(self):
+        pass
+
+    def __getattr__(self, name):
+        return getattr(sys.stderr, name)
 
 
-@app.route('/')
-def home_form():
-    return FORM_PAGE
-@app.route("/process", methods = ["GET", "POST"] )
-def process_form():
-    formData = request.values if request.method == "GET" else request.values
-    response = "Form Contents <pre>%s</pre>" % "<br/>\n".join(["%s:%s" % item for item in formData.items()] )
-    return response
-    return main.down()
-if __name__ == '__main__':
-    app.run()
+class Driver(PhantomJS):
+    def __init__(self, *args, **kwargs):
+        super(Driver, self).__init__(*args, **kwargs)
+        self._log = StderrLog()
+
+
+@app.route("/")
+def index():
+    url = request.args.get("url", "")
+    width = int(request.args.get("w", 1000))
+    min_height = int(request.args.get("h", 400))
+    wait_time = float(request.args.get("t", 20)) / 1000  # ms
+
+    if not url:
+        return "Example: <a href='http://127.0.0.1:5000/?url=http://nytimes.com'>" \
+               "Click here.</a>"
+
+    driver = Driver()
+    driver.set_window_position(0, 0)
+    driver.set_window_size(width, min_height)
+
+    driver.set_page_load_timeout(20)
+    driver.implicitly_wait(20)
+    driver.get(url)
+
+    driver.set_window_size(width, min_height)
+    time.sleep(wait_time)
+
+    sys.stderr.write(driver.execute_script("return document.readyState") + "\n")
+
+    png = driver.get_screenshot_as_png()
+    driver.quit()
+
+    return Response(png, mimetype="image/png")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
